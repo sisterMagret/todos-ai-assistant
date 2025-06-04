b8c014c49ebf3e96b67166059ed2455472824c69
sisterMagret <wistler4u@gmail.com>
2025-06-04 22:04:25 +0100

    added unused import in session.py

--- app/database/session.py
+++ app/database/session.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.pool import QueuePool
 
-from app.core.config import settings
+# from app.core.config import settings
 
 # DATABASE_URL = settings.DATABASE_URL
 
